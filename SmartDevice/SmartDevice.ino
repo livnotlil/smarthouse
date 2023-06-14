853211e42b9487188d6b5f2e17af88ce3ab1134d
lil <2160401@schoolsnet.act.edu.au>
2023-06-14 15:19:32 +1000

    Design and smart device

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -29,3 +29,66 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+void SecurityLights()
+
+// Traffic Lights - LED Outputs
+#define ledRed A0
+#define ledYellow A1 
+#define ledGreen A2
+
+//Potentiometer
+#define pot A3
+
+int potValue = analogRead(pot);            // reads the value of the potentiometer (value between 0 and 1023)
+
+// The lights will turn on depending on how close the person is. Using varibles and int parameters. 0-510 red, 511-1023 green (If greater than 510 turn on light)
+// if > 510
+  // Output to ledGreen (A2) (Pin 2)
+ // else 
+  //Output to ledRed (A0) (Pin 0)
+
+((//int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+//int brightness = potentiometerValue / 4;
+//analogWrite(LED_PIN, brightness);))
+
+
+ void TemperatureControledFan(){
+
+ // DC Motor & Motor Module - L298N
+motor.setSpeed(70);
+// Moisture Sensor
+pinMode(moisturePin, INPUT);
+
+//Depending on how high the humity is (via moisture sensor) changes how fast the fan is
+//int fan
+//loop 
+//Read humidity
+//If humidity <50 
+  //digitaWrite(M1, LOW)
+//else 
+  //digitalWrite(M1, HIGH) 
+
+void PanicButton(){
+ //When Button is pressed, Motor Module moves forwards and closes blinds and calls police, if button is pressed again, open blinds.
+
+int crashSensorValue = digitalRead(crashSensor);
+ 
+  motor.forward();
+delay(1000);
+motor.stop();
+delay(1000);
+motor.backward();
+delay(1000);
+}
+  //Read CrashSensor
+  //if button = pressed
+    //Loop{
+    // digital.Write(motor.forward);
+   // Call police (unsure how this would be coded){
+     // if button = pressed again
+       //digital.Write(motor.backward);
+}
+ 
+
+ 
--- design.md
+++ design.md
@@ -2,11 +2,18 @@
 
 ## Smart Device Theme
 
-> Include your theme here. Delete this comment.
+> Smart house
 
 
 ## Behaviours
 
 > Add Behaviours here. Delete this comment.
 
-1. Behaviour 1
+1. Outdoor lights:
+Depending on how close the user is the light will change. If the potentiometer reads the user above 510 the outdoor light will turn on. However if theyre under 510 the light will turn red and be dimmed.
+
+2. Humidity/moisture sensor
+If the humidity is below 50 the fan will be turned on on low, otherwise the fan will be high speed
+
+3. Panic Button:
+if the button is pressed the motor module will close the blinds and call the police, if the button is pressed again soon after it will reopen the blinds (resetting the button)
